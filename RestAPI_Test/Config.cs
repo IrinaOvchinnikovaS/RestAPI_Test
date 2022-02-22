a2c8cec5b9c5f3b0ef8800546988ec61f7924bf6
Irina <irina.s.tomilova@yandex.ru>
2022-02-22 12:16:39 +0500

    Добавлен класс конфигурации

---
+++ RestAPI_Test/Config.cs
@@ -0,0 +1,24 @@
+﻿namespace RestAPI_Test
+{
+    public class Config
+    {
+        private IConfiguration _appConfiguration { get; set; }
+        public string _resultFilePath { get; set; }
+
+        public Config(IConfiguration AppConfiguration)
+        {
+            _appConfiguration = AppConfiguration;
+
+            _resultFilePath = ReadResultFilePath();
+        }
+
+        private string ReadResultFilePath()
+        {
+            string newsContentPath = _appConfiguration.GetSection("PathToResultFolder").Value;
+
+            return (string.IsNullOrEmpty(newsContentPath) || newsContentPath == "\\")
+                ? Directory.GetCurrentDirectory()
+                : newsContentPath;
+        }
+    }
+}
